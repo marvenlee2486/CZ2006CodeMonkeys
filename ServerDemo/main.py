4b66e3eca619dacb5acf70c0d3149b7e51a9dd8f
pityhero233 <shizhengyu93@hotmail.com>
2022-03-25 02:57:36 +0800

    bugfix

--- ServerDemo/main.py
+++ ServerDemo/main.py
@@ -1,7 +1,7 @@
 from geotool import *
-from dbConnector import dbConnector, initDB
-from tcpManager import TCPManager, initTCP, initiateSaveMeRequest, tcpdaemon, onlineEvents
-from websocketManager import websocketsManager, initWebsocket
+from dbConnector import initDB
+from tcpManager import initTCP
+from websocketManager import initWebsocket
 
 if __name__ == "__main__":
     initDB(); initTCP(); initWebsocket()
