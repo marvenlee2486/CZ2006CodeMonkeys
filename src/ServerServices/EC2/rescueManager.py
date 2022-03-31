a13e1503178479cdd798fb1322c1a0bc86128496
pityhero233 <shizhengyu93@hotmail.com>
2022-04-01 04:27:14 +0800

    massive rework of server. untested

--- src/ServerServices/EC2/rescueManager.py
+++ src/ServerServices/EC2/rescueManager.py
@@ -1,14 +1,31 @@
+from dataclasses import dataclass
 import socket
 import datetime 
+import typing
 from threading import Thread
 from geotool import geoDistance
 ALARM_DISTANCE_THRESHOLD = 5000
-# we don't dynamically maintain who we send request to.
 
-class rescueManager: # perform all TCP Requests
+@dataclass
+class Event:
+    patientSck: socket.socket
+    accept: set[str]
+    decline: set[str]
+    informed: set[str]
+    patientLat: str
+    patientLon: str
+    time: str
+
+@dataclass
+class ConnectedUser:
+    sck: socket.socket
+    lat: str
+    lon: str
+
+class rescueManager: 
     sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connectedUsers = {} # telephone: (socketHandle, lat, lon)
-    events = {} # patientId: (patientSck, [accept], [decline], patientLat, patientLon, time)
+    connectedUsers: dict[str, ConnectedUser] = {} # telephone: (socketHandle, lat, lon); notice this only stores connected volunteers; socket for connected patients due to emergency are stored in events
+    events: dict[str, Event] = {}
     def run(self):
         print("TCPManager started.")
         self.sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,7 +35,7 @@ class rescueManager: # perform all TCP Requests
             newSck, addr = self.sck.accept()
             print("new TCP client.")
             Thread(target=rescueManager.clientRequestHandler, args=(self, newSck, addr)).start()
-
+    
     def clientRequestHandler(self, clientSocket, addr): # receives user location
         while True:
             try:
@@ -28,25 +45,34 @@ class rescueManager: # perform all TCP Requests
 
                 if msg.startswith('ACCEPTREQ'):
                     [mode, patientTel, volTel] = msg.split(";")
-                    self.events[patientTel][0].append(volTel)
+                    self.events[patientTel].accept.add(volTel)
                     self.updateIncomingAmount(patientTel)
 
-                elif msg.startswith('LOCATION'):
+                elif msg.startswith('LOCATION'): # new update to location
                     [mode, tel, lat, lon] = msg.split(";")
-                    self.connectedUsers[tel] = (clientSocket, lat, lon)
+                    self.connectedUsers[tel] = ConnectedUser(clientSocket, lat, lon)
+                    for patientTel in self.events: # see if there is anything he can do
+                        tp = self.events[patientTel] # current event
+                        if geoDistance(tp.patientLat, tp.patientLon, lat, lon) <= ALARM_DISTANCE_THRESHOLD and tel not in tp.informed:
+                                tp.informed.add(tel) # add to informed
+                                message = ['REQUEST', patientTel, tp.patientLat, tp.patientLon]
+                                clientSocket.send(';'.join(message))
+                    
                     print("User location updated.")
 
                 elif msg.startswith('DECLINEREQ'):
                     [mode, patientTel, volTel] = msg.split(";")
-                    self.events[patientTel][1].append(volTel)
+                    self.events[patientTel].decline.add(volTel)
                     self.updateIncomingAmount(patientTel)
                 
                 elif msg.startswith('RESCUEME'):
-                    [mode, name, telephone, lat, lon] = msg.split(';')
-                    # create new event entry
-                    self.events[telephone] = (clientSocket, [], [], lat, lon, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                    [mode, telephone, patientLat, patientLon] = msg.split(';')
+                    # create new event entry, overwriting previous one if any
+                    if telephone in self.events:
+                        print("Warning: previous event of same patient not closed, overwriting previous event")
+                    self.events[telephone] = Event(clientSocket, accept = set(), decline = set(), informed = set(), patientLat = lat, patientLon = lon, time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                     # compose emergency message
-                    message = ['REQUEST', name, telephone, lat, lon]
+                    message = ['REQUEST', telephone, patientLat, patientLon]
                     # # if location is near home, set to home
                     # if geoDistance(float(homeLat), float(homeLon), float(lat), float(lon)) < 100.0:
                     #     message.append(homeAddrDesc)
@@ -55,26 +81,30 @@ class rescueManager: # perform all TCP Requests
                     # filter available individuals
                     numAvailable = 0
                     for tel in self.connectedUsers:
-                        (tsck, tlat, tlon) = self.connectedUsers[tel]
-                        if geoDistance(tlat, tlon, lat, lon) <= ALARM_DISTANCE_THRESHOLD: 
-                            tsck.send(';'.join(message))
+                        tp = self.connectedUsers[tel]
+                        if geoDistance(tp.lat, tp.lon, patientLat, patientLon) <= ALARM_DISTANCE_THRESHOLD: 
+                            tp.sck.send(';'.join(message).encode('utf-8'))
                             numAvailable += 1
                     print("A new emergency request received. %d volunteers online. %d volunteers fulfilled requirement." % (len(self.connectedUsers), numAvailable))
-            
+                elif msg.startswith('CANCELRESCUEME'): # either finish or manual cancel
+                    [mode, telephone] = msg.split(';')
+                    if telephone not in self.events:
+                        print("Unexpected: client try to cancel a nonexist event.")
+                    else:
+                        for tTel in self.events[telephone].informed:
+                            try: self.connectedUsers[telephone].sck.send(msg.encode('utf-8'))
+                            except: print("fail to send cancel operation info for one of the volunteers. error is ignored.")   
                 else:
                     print("Malformed input data. Missing request type.")
             except:
                 print("Malformed input data. Missing request argument.")
 
-    def updateIncomingAmount(self, tel): # update everyone involved in event an update of number of volunteers participated, event is characterised by telephone of patient.
-        message = ['UPDATERESCUERS', len(self.events[tel][1]), len(self.events[tel][2])]
-        for tTel in self.events[tel][1]:
-            try:
-                self.connectedUsers[tTel][0].send(';'.join(message))
-            except:
-                print("Unable to push update to a certain volunteer. the error is ignored.")
-        self.events[tel][0].send(';'.join(message)) # update patient himself
-        
+    def updateIncomingAmount(self, tel): # update detail to everyone informed
+        message = ['UPDATERESCUERS', tel, len(self.events[tel].accept)] 
+        for tTel in self.events[tel].informed:
+            try: self.connectedUsers[tel].sck.send(';'.join(message).encode('utf-8'))
+            except: print("fail to reach one of the volunteers while updating information. error is ignored.")
+    
 def initTCP():
     global rescueDaemon, onlineEvents
     rescueDaemon = rescueManager()
