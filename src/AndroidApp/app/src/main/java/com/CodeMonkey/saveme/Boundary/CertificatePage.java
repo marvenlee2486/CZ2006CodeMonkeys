5d0f2e8907e97b9129a56b210c6212f2bc22961c
Luo Yihang <luo_yihang@outlook.com>
2022-04-04 01:01:52 +0800

    [Android]: Fix Certificate Show Bug

--- src/AndroidApp/app/src/main/java/com/CodeMonkey/saveme/Boundary/CertificatePage.java
+++ src/AndroidApp/app/src/main/java/com/CodeMonkey/saveme/Boundary/CertificatePage.java
@@ -2,11 +2,15 @@ package com.CodeMonkey.saveme.Boundary;
 
 
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -25,7 +29,7 @@ import java.net.URL;
 public class CertificatePage extends BaseActivity {
     private TextView mainName;
     private ImageView certificateImage;
-
+    private Bitmap bitmap;
     @Override
      public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,40 +45,56 @@ public class CertificatePage extends BaseActivity {
         certificateImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, 1);
+                showDialog(bitmap);
             }
         });
+
     }
 
     private void DisplayPage(){
         mainName.setText(UserController.getUserController().getUser().getName());
 
-        Bitmap bmp = getImageBitmap(CertificateController.getInstance().getCertificateUrl());
-        certificateImage.setImageBitmap(bmp);
+        new DownloadImageTask(certificateImage)
+                .execute(CertificateController.getInstance().getCertificateUrl());
     }
 
-    public Bitmap getImageBitmap(String url) {
-        Log.e("Certificate", url);
-        URL imgUrl = null;
-        Bitmap bitmap = null;
-        try {
-            imgUrl = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) imgUrl
-                    .openConnection();
-            conn.setDoInput(true);
-            conn.connect();
-            InputStream is = conn.getInputStream();
-            bitmap = BitmapFactory.decodeStream(is);
-            is.close();
-        } catch (Exception e) {
-            Log.e("Certificate", e.toString());
-            e.printStackTrace();
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
         }
-        return bitmap;
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 
+    private void showDialog(Bitmap bitmap){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.certificate_show_layout);
+        ImageView imageView = dialog.findViewById(R.id.certificatePhoto);
+        imageView.setImageBitmap(bitmap);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+        dialog.setCanceledOnTouchOutside(true);
+    }
 
 }
