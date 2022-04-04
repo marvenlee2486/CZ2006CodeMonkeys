a0132b6671b2569a1beaa245dc2f5f31af6ca017
Ubuntu <ubuntu@ip-172-31-27-73.ap-southeast-1.compute.internal>
2022-04-04 07:52:19 +0000

    yup

--- src/ServerServices/EC2/websocketManager.py
+++ src/ServerServices/EC2/websocketManager.py
@@ -10,7 +10,7 @@ class websocketsManager:
         while True:
             try:
                 async with websockets.connect("wss://chfo8hmaua.execute-api.ap-southeast-1.amazonaws.com/production") as websocket:
-                    websocket.send('''{"action":"setName","name":"laganma"}''') # auth
+                    await websocket.send('''{"action":"setName","name":"laganma"}''') # auth
                     while True:
                         extractedEvents = []
                         for tel in rescueManager.rescueDaemon.events:
@@ -18,7 +18,8 @@ class websocketsManager:
                             te = {"patientTel": tel, "patientLat": x.patientLat, "patientLon": x.patientLon, "accept": x.accept, "decline": x.decline, "informed": x.informed, "startTime": x.time, "endTime": x.endTime} 
                             extractedEvents.append(te)
                         obj = {"action": "sendPrivate", "to": "admin", "message": extractedEvents}
-                        websocket.send(json.dumps(obj))
+                        print("Websocket: reporting %s" % (json.dumps(obj)))
+                        await websocket.send(json.dumps(obj))
                         await asyncio.sleep(15)
             except:
                 print("temporarily lost connection to websocket broadcaster. try to reconnect.")
